c4c1c93ce073cd9910e1549f015ecabc6bf2a55a
Jasmine Lao <jsmn.lao@gmail.com>
2025-04-11 22:57:31 -0700

    POST in auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email_login')
+        password = request.form.get('password_login')
+
+        # validate email and password
+        # if true, login user to account
+
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        name = request.form.get('name_signup')
+        email = request.form.get('email_signup')
+        password = request.form.get('password_signup')
+
+        # add user to database
+
     return render_template("signup.html")
