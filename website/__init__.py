ba63ac76c5f93e0b5517360a0f7a40387c61bec4
Jasmine Lao <jsmn.lao@gmail.com>
2025-04-18 16:48:01 -0700

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "travelbuddy.db"
@@ -23,5 +24,13 @@ def create_app():
         if not path.exists('website/' + DB_NAME):
             db.create_all()
             print("Database created successfully")
+
+    login_manager = LoginManager()
+    login_manager.longin_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
