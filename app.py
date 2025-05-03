2790c404ea142d88a795d8693dbb1c25dc15923c
Jasmine Lao <jsmn.lao@gmail.com>
2025-05-03 12:20:07 -0700

    fixed overview formatting

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from website import create_app
 
 app = create_app()
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
